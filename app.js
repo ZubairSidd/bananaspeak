83ed999cc05079af6e81bf45b807c5ccac320ac1
Zubair Siddique <zubairsidd20@gmail.com>
2021-04-04 09:37:07 +0530

    upto ex05

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ var btnTranslate = document.querySelector("#click");
 btnTranslate.addEventListener("click",function clickEventHandler(){
     console.log("clicked")
 })
+
+var copyBtn = document.querySelector("#copy");
+var textArea = document.querySelector("#input")
+var outputPlace = document.querySelector("#output")
+
+copyBtn.addEventListener("click",function clickEventHandler(){
+    outputPlace.innerText = textArea.value
+})
