d5f4c8916b1456e7109cf10cfdfb77397446ae47
Zubair Siddique <zubairsidd20@gmail.com>
2021-04-05 09:28:38 +0530

    javascript done, translator working

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 var copyBtn = document.querySelector("#copy");
 var textArea = document.querySelector("#input")
 var outputPlace = document.querySelector("#output")
+var url = "https://api.funtranslations.com/translate/minion.json?"
 
-function clickEventHandler(){
-    outputPlace.innerText = textArea.value
+
+
+function getTranslationURL(text){
+    return text = url + "text=" + text
+}
+function errorHandler(){
+    console.log("error occured", error)
+    alert("something wrong with server! try again after some time")
+}
+function clickEventHandler() {
+    var inputText = textArea.value 
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => outputPlace.innerText = json.contents.translated)
 }
 
 copyBtn.addEventListener("click",clickEventHandler)
